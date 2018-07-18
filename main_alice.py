dc602facf40fbb93b4e1cc33b26020379f7360d4
Evsey Antonovich <evseyru1@mail.ru>
2018-07-18 23:23:51 +0300

    Added a fighting module to the bot.

--- main_alice.py
+++ main_alice.py
@@ -1,7 +1,7 @@
 # Импортируем модули для работы с JSON и логами.
 import json
 import logging
-
+import random
 # Импортируем подмодули Flask для запуска веб-сервиса.
 from flask import Flask, request
 app = Flask(__name__)
@@ -55,8 +55,16 @@ def handle_dialog(req, res):
                 "random",
                 "facerec",
                 "help",
-                "words"
-            ]
+                "words",
+                "fight"
+            ],
+            'fightMode': False,
+            'player_hp': 30,
+            'player_mp': 10,
+            'enemy_hp':45,
+            'enemy_mp':20,
+            'name':random.choice(config_json["name1"])+random.choice(config_json["name2"])
+
         }
 
         res['response']['text'] = config_json["greet"]
@@ -65,10 +73,73 @@ def handle_dialog(req, res):
 
     # Обрабатываем ответ пользователя.
     command = req['request']['original_utterance'].split()
-    if command[0] in list(config_json["commands"].keys()):
+    if command[0] == "fight":
+        res['response']['text'] = str()
+        sessionStorage[user_id]['name'] = str(random.choice(config_json["name1"])+ " "+random.choice(config_json["name2"]))
+        sessionStorage[user_id]['player_hp'] = 30
+        sessionStorage[user_id]['player_mp'] = 10
+        sessionStorage[user_id]['enemy_hp'] = 45
+        sessionStorage[user_id]['enemy_mp'] = 20
+        sessionStorage[user_id]['fightMode'] = True
+        sessionStorage[user_id]['suggests'] = [
+            "stats",
+            "attack",
+            "heal",
+            "charge",
+            "stop"
+        ]
+        res['response']['text'] = str(random.choice(config_json['nameaction']) +" "+ sessionStorage[user_id]['name']+"! " + sessionStorage[user_id]['name'] + "'s HP: "+
+                        str(sessionStorage[user_id]['enemy_hp']) + ' Your HP: ' + str(sessionStorage[user_id][
+                        'player_hp']) + ' Your MP: ' + str(sessionStorage[user_id]['player_mp']))
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+    if sessionStorage[user_id]['fightMode']:
+        if command[0] in ["stats", "attack", "heal", "charge", "stop"]:
+            if command[0] == "stats":
+                res['response']['text'] = str(
+                    sessionStorage[user_id]['name'] + "'s HP: "+
+                        str(sessionStorage[user_id]['enemy_hp']) + ' Your HP: ' + str(sessionStorage[user_id][
+                        'player_hp']) + ' Your MP: ' + str(sessionStorage[user_id]['player_mp']))
+                res['response']['buttons'] = get_suggests(user_id)
+                return
+            else:
+                if command[0] == "attack":
+                    attackamt = random.randint(2,6)
+                    sessionStorage[user_id]['enemy_hp'] -= attackamt
+                    res['response']['text'] = str("You attack "+sessionStorage[user_id]['name']+" for "+str(attackamt)+ " damage! "
+                                                  + sessionStorage[user_id]['name'] + "'s HP: " + str(sessionStorage[user_id]['enemy_hp'])+" ")
+                elif command[0] == "heal":
+                    pointamt = random.randint(1,3)
+                    healamt = random.randint(6,8)
+                    sessionStorage[user_id]['player_hp'] += healamt
+                    sessionStorage[user_id]['player_mp'] -= pointamt
+                    res['response']['text'] = str("You use "+str(pointamt)+ " magic to heal for "+str(healamt)+" damage! Your HP: "+
+                                                  str(sessionStorage[user_id]['player_hp'])+ " Your MP: "+str(sessionStorage[user_id]['player_mp'])+" ")
+                elif command[0] == "charge":
+                    chargeamt = random.randint(4,7)
+                    sessionStorage[user_id]['player_mp'] += chargeamt
+                    res['response']['text'] = str("You regain "+str(chargeamt)+" points! ")
+
+                elif command[0] == "stop":
+                    sessionStorage[user_id]['fightMode'] = False
+                    res['response']['text'] = str("You stop the battle.")
+                    return
+                action = arcadeAction(user_id)
+                res['response']['text'] += action[5]
+                sessionStorage[user_id]['player_hp'] += action[0]
+                sessionStorage[user_id]['player_mp'] += action[1]
+                sessionStorage[user_id]['enemy_hp'] += action[2]
+                sessionStorage[user_id]['enemy_mp'] += action[3]
+                res['response']['buttons'] = get_suggests(user_id)
+                return
+
+
+
+    elif command[0] in list(config_json["commands"].keys()):
         cmd = config_json["commands"][command[0]]["func"]
         cmdvalue = getattr(globals()[cmd], "main")(*command[1:])
         res['response']['text'] = cmdvalue
+        res['response']['buttons'] = get_suggests(user_id)
         return
     #if req['request']['original_utterance'].lower() in [
     #    'ладно',
@@ -98,7 +169,7 @@ def get_suggests(user_id):
 
     # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
     #session['suggests'] = session['suggests'][1:]
-    sessionStorage[user_id] = session
+    #sessionStorage[user_id] = session
 
     # Если осталась только одна подсказка, предлагаем подсказку
     # со ссылкой на Яндекс.Маркет.
@@ -110,3 +181,45 @@ def get_suggests(user_id):
     #    })
 
     return suggests
+def arcadeAction(user_id):
+    enemy_hp = sessionStorage[user_id]["enemy_hp"]
+    enemy_mp = sessionStorage[user_id]["enemy_mp"]
+    player_hp = sessionStorage[user_id]["player_hp"]
+    player_mp = sessionStorage[user_id]["player_mp"]
+    message = ""
+    pHP = 0
+    pMP = 0
+    eHP = 0
+    eMP = 0
+    eName = sessionStorage[user_id]['name']
+    mode = 0
+    if (enemy_hp <= 0 or enemy_mp <= 0):
+        mode = "win"
+        message = "You kill "+eName+ "! Congratulations!"
+        sessionStorage[user_id]["fightMode"] = False
+    elif (enemy_mp <= 5 and random.randint(0,100) <= 70):
+        mode = "steal"
+        stealamt = random.randint(2,3)
+        pMP -= stealamt
+        message = eName + " steals "+str(stealamt)+" of your mana!"
+    elif (enemy_hp <= 10 and enemy_mp > 4):
+        mode = "heal"
+        message = eName + " heals for 4 health!"
+        eHP += 4
+        eMP -= 4
+    else:
+        attackamt = random.randint(3,6)
+        mode = "attack"
+        message = eName + " attacks for "+str(attackamt)+" damage!"
+        pHP -= attackamt
+    if (pHP != 0):
+        message += " Your HP: " + str(player_hp + pHP)
+    if (pMP != 0):
+        message += " Your MP: " + str(player_mp + pMP)
+    if (eHP != 0 and mode != "win"):
+        message += " "+eName+ "'s HP: " + str(enemy_hp + eHP)
+    if (player_hp + pHP <= 0 or player_mp + pMP <= 0):
+        mode = "gameover"
+        message += " You die! Game over!"
+        sessionStorage[user_id]["fightMode"] = False
+    return(pHP,pMP,eHP,eMP,mode,message)
